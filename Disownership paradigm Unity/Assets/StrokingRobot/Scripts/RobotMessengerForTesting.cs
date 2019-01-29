149e796f4a2c6770996f90e7db9b537269d4e13f
Marte Roel <marteroel@users.noreply.github.com>
2019-01-29 14:30:47 +0100

    added StrokingRobot unitypackage, tested

---
+++ Disownership paradigm Unity/Assets/StrokingRobot/Scripts/RobotMessengerForTesting.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMessengerForTesting : MonoBehaviour {
+
+	public SerialController serialManager;
+
+	[Range(0,100)]
+	public int distance;
+	[Range(2,16)]
+	public int speed;
+	[Range(0,90)]
+	public int angle;
+
+
+	private int storedDistance, storedSpeed, storedAngle;
+	private string error;
+	private bool initialized;
+
+	void Start() {
+		storedDistance = distance;
+		storedSpeed = speed;
+		storedAngle = angle;
+
+	}
+
+	//SEND MOVEMENT SEGMENT SHOULD BE SENT N NUMBER OF TIMES, IN ORDER TO SUM 100 UNITS OF DISTANCE.
+	//AFTER ALL THE SEGMENTS ARE SENT, THE STARTMOVEMENT() SHOULD BE CALLED
+	//CLEAR NUMBER SEGMENT SHOULD BE CALLED BEFORE APPLYING A NEW SET OF MOVEMENT SEGMENTS.
+		
+	void Update(){
+
+
+		/*
+		if (serialManager.ReadFromPort (5) != null){// | serialManager.ReadFromPort (5) != " ") {
+			error = serialManager.ReadFromPort (5);
+
+			if (error != null)
+				Debug.Log ("The stroker says: " + error);
+		}*/
+
+		if (error == "ACK")
+			initialized = true;
+		
+		if(Input.GetKeyDown("space")) SendMovementSegment (distance, speed, angle); //if (distance != storedDistance || speed != storedSpeed || angle != storedAngle)
+
+		if (Input.GetKeyDown ("s"))		StartMovement ();
+
+		if (Input.GetKeyDown ("c")) 	ClearNumberSegment ();
+
+		if (Input.GetKeyDown ("r")) 	ReadSegment ();
+
+		if (Input.GetKeyDown ("t"))		ReadDuration();
+	}
+
+	public void SendMovementSegment(int distance, int speed, int angle){
+
+		string message = ("SET,L" + distance.ToString("000") + ",MS" + speed.ToString("00") + ",SV" + angle.ToString("00") + ",R" + speed.ToString("00")); //("00") sets the number of digits to 2
+
+		if (distance < 0 || distance > 100)
+			Debug.LogError ("wrong distance value, should be between 0 and 100");
+		else if (speed < 2 || speed > 16)
+			Debug.LogError ("wrong speed value, should be between 2 and 16");
+		else if (angle < 0 || angle > 180)
+			Debug.LogError ("wrong angle value, should be between 0 and 180");
+
+		else 
+			serialManager.SendSerialMessage (message);
+	}
+
+	public void ClearNumberSegment(){
+		serialManager.SendSerialMessage ("CLEAR");
+	}
+
+	public void StartMovement(){
+		serialManager.SendSerialMessage ("START");
+	}
+
+	public void ReadSegment(){
+		serialManager.SendSerialMessage ("READ");
+	}
+
+	public void ReadDuration(){
+		serialManager.SendSerialMessage ("TIME");
+	}
+
+
+}
---
+++ Disownership paradigm Unity/Assets/StrokingRobot/Scripts/SerialManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+
+public class SerialManager : MonoBehaviour {
+
+	[Tooltip("The name of the serial port")]
+	public string portName;//the COM Port
+
+	public int baudRate = 115200;//Fixed to 115200 for the robot.
+	SerialPort serialDevice;
+
+
+	void Start () {
+		serialDevice = new SerialPort (portName, baudRate); //initializes a serial port
+		if(serialDevice != null) serialDevice.Close (); //makes sure the device is closed before openning
+		serialDevice.Open (); //opens serial device
+
+
+	//	StartCoroutine (AsynchronousReadFromPort ((string s) => Debug.Log (s), () => Debug.LogError ("Error!"), 10000f));
+	}
+
+	void Update(){
+		
+	}
+
+	public void WriteToPort(string message){
+		if (serialDevice.IsOpen) {
+			serialDevice.Write (message); //if the device is open, send string message when function is called.
+			Debug.Log("sent message " + message); //writes message to console (this does not confirm that it was received)
+		}
+	}
+
+
+	//Read from arduino
+	public string ReadFromPort(int timeout = 0) {
+
+		serialDevice.ReadTimeout = timeout;
+
+		try {
+			return serialDevice.ReadLine();
+		}
+
+		catch (TimeoutException) {
+			return null;
+		}
+	}
+		
+
+	public IEnumerator AsynchronousReadFromPort(Action<string> callback, Action fail = null, float timeout = float.PositiveInfinity) {
+		DateTime initialTime = DateTime.Now;
+		DateTime nowTime;
+		TimeSpan diff = default(TimeSpan);
+
+		string dataString = null;
+
+		do {
+			try {
+				dataString = serialDevice.ReadLine();
+			}
+			catch (TimeoutException) {
+				dataString = null;
+			}
+
+			if (dataString != null)
+			{
+				callback(dataString);
+				yield break; // Terminates the Coroutine
+			} else
+				yield return null; // Wait for next frame
+
+			nowTime = DateTime.Now;
+			diff = nowTime - initialTime;
+
+		} while (diff.Milliseconds < timeout);
+
+		if (fail != null)
+			fail();
+		yield return null;
+	}
+
+
+
+	void OnDisable() {
+		serialDevice.Close(); //close device when finished.
+	}
+}
