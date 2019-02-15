f6dcfcf564c7c15e077d691014d04d9f03a161b0
marteroel <marteroel@users.noreply.github.com>
2019-02-15 15:07:00 +0100

    Now has autocalibration at the beginning

---
+++ Disownership paradigm Unity/Assets/Scripts/DefaultCalibration.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleVAS;
+
+public class DefaultCalibration : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (BasicDataConfigurations.useCalibration)
+            UnityEngine.XR.InputTracking.Recenter();
+    }
+	
+}
