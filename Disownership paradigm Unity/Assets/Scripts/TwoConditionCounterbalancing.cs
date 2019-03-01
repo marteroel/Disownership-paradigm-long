cb0f5d8b72a7d792fc73d5ea3baf9d82d7a7f9dc
marteroel <marteroel@users.noreply.github.com>
2019-03-01 11:39:24 +0100

    Added udp connection from ui and simple condition switcher for two order-counterbalancing

---
+++ Disownership paradigm Unity/Assets/Scripts/TwoConditionCounterbalancing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TwoConditionCounterbalancing : MonoBehaviour {
+
+    public InputField[] delayPerCondition;
+    public Toggle orderToggle;
+    private float[] order1 = { 0f, 1f, 0f, 1f, 0f, 1f };
+    private float[] order2 = { 1f, 0f, 1f, 0f, 1f, 0f };
+
+    public void SetConditionOrder() {
+
+        Debug.Log("setting condition order");
+
+        float[] selectedOrderArray;
+
+        if (!orderToggle.isOn)
+            selectedOrderArray = order1;
+        else
+            selectedOrderArray = order2;
+
+        for (int i = 0; i < delayPerCondition.Length; i++) {
+            Debug.Log(selectedOrderArray[i]);
+            delayPerCondition[i].text = selectedOrderArray[i].ToString();
+            
+        }
+    }
+}
