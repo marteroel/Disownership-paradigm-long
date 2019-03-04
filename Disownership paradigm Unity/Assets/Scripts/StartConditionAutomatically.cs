a4e4625db08f33f245025871f97a87877a855be2
Body-self-and-plasticity-Lab <33545780+BSP-Lab@users.noreply.github.com>
2019-03-04 14:25:40 +0100

    Now loads stimulation automatically, tested

---
+++ Disownership paradigm Unity/Assets/Scripts/StartConditionAutomatically.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleVAS;
+
+public class StartConditionAutomatically : MonoBehaviour {
+
+	public LoadScene sceneLoader;
+	public float time;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (LoadConditionAtTime (time));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private IEnumerator LoadConditionAtTime (float time){
+		yield return new WaitForFixedTime (time);
+		sceneLoader.OnNextButton ();
+
+	}
+}
